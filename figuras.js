517135c3c532170776991dcc71035d73a25b324d
pipe <91910334+pipipe123@users.noreply.github.com>
2021-10-21 11:58:55 -0500

    finalizado

---
+++ figuras.js
@@ -0,0 +1,14 @@
+
+
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+    const value =  input.value;
+
+    const perimetro = (value*4);
+    alert (perimetro);
+}
+
+function calcularAreaCuadrado(){
+
+}
